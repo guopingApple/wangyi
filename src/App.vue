6d4fad044d632c015c18c7cadad8fbf5fa18457f
guopingApple <369854279@qq.com>
2017-08-09 18:36:39 +0800

    163

--- src/App.vue
+++ src/App.vue
@@ -6,34 +6,28 @@
 					<div>网易</div>
 					<div slot='right'>搜索</div>
 				</x-header>
-				
-				<scroller :lock-y='true'>
-					<div class="tab">
-						<tab>
-								<tab-item selected>推荐</tab-item>
-								<tab-item>要闻</tab-item>
-								<tab-item>游戏</tab-item>
-								<tab-item>科技</tab-item>
-								<tab-item>体育</tab-item>
-								<tab-item>互联网</tab-item>
-						</tab>
-					</div>
-				</scroller>
-				
-				<swiper :list='swiperList' v-model = 'swiperItemIndex' :loop='true'>
-						
-				</swiper>
-				
-				<marquee class="my-marquee">
-					<marquee-item>1111</marquee-item>
-					<marquee-item>2222</marquee-item>
-					<marquee-item>333</marquee-item>
-					<marquee-item>444</marquee-item>
-				</marquee>
-				
-				<panel :list ='datalist'>
+				<sc :lock-y='true'>
+						<div class="tab">
+							<tab>
+									<tab-item selected>推荐</tab-item>
+									<tab-item>要闻</tab-item>
+									<tab-item>游戏</tab-item>
+									<tab-item>科技</tab-item>
+									<tab-item>体育</tab-item>
+									<tab-item>互联网</tab-item>
+							</tab>
+						</div>
+					</sc>
+				<scroller class='my-scroller' :on-refresh = 'refresh' :on-infinite = 'infinite' ref = 'myRef'>
+					<swiper :list='swiperList' v-model = 'swiperItemIndex' :loop='true'></swiper>
 					
-				</panel>
+					<marquee class="my-marquee">
+						<marquee-item v-for = "list in marqueelist" :key="list.typeid">{{list.title}}</marquee-item>
+					</marquee>
+					
+					<panel :list ='datalist'></panel>
+					
+				</scroller>
 				
 				<tabbar slot = 'bottom'>
 					<tabbar-item>
@@ -57,8 +51,19 @@
 
 <script>
 
-import { ViewBox,XHeader,Tabbar, TabbarItem,Tab, TabItem,Scroller,Swiper,Marquee, MarqueeItem,Panel} from 'vux'
-
+import { ViewBox,XHeader,Tabbar, TabbarItem,Tab, TabItem,Scroller as sc,Swiper,Marquee, MarqueeItem,Panel} from 'vux'
+var refreshKey = ['A','B01','B02','B03','B04','B05','B06','B07','B08','B09']
+var key = 0;
+function getRefreshKey(){
+	var keyValue = refreshKey[key];
+	key++;
+	if(key == refreshKey.length){
+		console.log(refreshKey.length);
+		console.log(keyValue);
+		key = 0;
+	}
+	return keyValue;
+}
 export default {
   name: 'app',
   components:{
@@ -66,38 +71,90 @@ export default {
   	XHeader,
   	Tabbar, TabbarItem,
   	Tab, TabItem,
-  	Scroller,
+  	sc,
   	Swiper,
   	Marquee, MarqueeItem,
   	Panel
   },
+  created(){
+  	// http://3g.163.com/touch/jsonp/sy/recommend/0-9.html
+  	this.$jsonp('http://3g.163.com/touch/jsonp/sy/recommend/0-9.html').then( data => {
+		console.log(data);
+
+			// 轮播图的数据
+			this.swiperList = data.focus.filter(item=>{
+				return item.addata ===null;
+			}).map(item =>{
+				return{
+					url:item.link,
+					img:item.picInfo[0].url,
+					title:item.title
+				}
+			})
+			
+			//滚动新闻数据
+			this.marqueelist = data.live.filter(item=>{
+				return item.addata ===null;
+			}).map(item =>{
+				return{
+					title:item.title,
+				}
+			})
+			
+			//首屏新闻列表的数据
+			this.datalist = data.list.filter(item=>{
+				return item.addata ===null;
+			}).map(item =>{
+				return{
+					src:item.picInfo[0].url,
+					title:item.title,
+					desc:item.digest,
+					url:item.link
+				}
+			})
+			
+  	})
+  },
   data(){
-  	var datalist = [];
-  	for (var i = 0;i<10;i++) {
-  		datalist.push({
-  			src: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
-        title: '标题一',
-        desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
-        url: '/component/cell'
-  		})
-  	}
+  	
   	return{
-  		swiperList:[{
-			  url: 'javascript:',
-			  img: 'https://static.vux.li/demo/1.jpg',
-			  title: '送你一朵fua'
-			}, {
-			  url: 'javascript:',
-			  img: 'https://static.vux.li/demo/2.jpg',
-			  title: '送你一辆车'
-			}, {
-			  url: 'javascript:',
-			  img: 'https://static.vux.li/demo/3.jpg',
-			  title: '送你一次旅行'
-			}],
+  		swiperList:[],
 			swiperItemIndex :0,
-			type: '1',
-      datalist: datalist
+      datalist: [],
+      marqueelist:[]
+  	}
+  	
+  },
+  methods:{
+  	refresh(){
+//		console.log(1);
+			//下拉数据刷新
+			this.$jsonp('http://3g.163.com/touch/jsonp/sy/recommend/0-9.html',{
+				miss:'00',
+				refresh:getRefreshKey()
+			}).then( data => {
+//				console.log(data)
+
+					//刷新首屏新闻列表的数据
+					this.datalist = data.list.filter(item=>{
+						return item.addata ===null;
+					}).map(item =>{
+						return{
+							src:item.picInfo[0].url,
+							title:item.title,
+							desc:item.digest,
+							url:item.link
+						}
+					});
+					
+					this.$refs.myRef.finishPullToRefresh();
+					
+			})
+			
+  	},
+  	infinite(){
+//		console.log(2);
+			
   	}
   }
 }
@@ -117,6 +174,13 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   height: 100%;
+
+	.vux-header{position: absolute;width: 100%;left: 0;top: 0;}
+	.tab{width: 600px;margin-top: 46px;}
+	.my-marquee{margin-top: 10px;}
+	.my-scroller{top: 90px;}
+	.loading-layer{
+		padding-bottom: 90px;
+	}
 }
-.tab{width: 600px;}
 </style>
